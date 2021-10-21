c4908736b0b325d05af968f34e1c5b4db648e933
Brian <brianmccollum@unm.edu>
2021-10-21 01:18:31 -0600

    giga chad level start

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -2,4 +2,5 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	OS.window_fullscreen = true
 	get_tree().paused = true
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,18 +1,14 @@
 extends KinematicBody2D
 onready var animated_sprite = $AnimatedSprite
-onready var hitbox = $Hitbox
 
 var velocity = Vector2.ZERO # set velocity to o,o
 var max_run = 800 # max left and right speed
 var run_accel = 3800 # how fast you accelerate left and right
-var gravity = 6000 # overall gravity modifier (both jump and fall)
-var max_fall = 380 * 4# fall speed
-var jump_force = -240 * 4 # how high you jump
-var jump_hold_time = 0.1 # how long you can hold jump to get extra height
-var fly_force = 600 * 4
-var fly_hold_time = 0.1
-var local_hold_time = 0
-var cur_jumps = 5
+var gravity = 5000 # overall gravity modifier (both jump and fall)
+var max_fall = 6000 # fall speed
+var jumps = 5
+var fly_force
+var can_fly = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -25,16 +21,21 @@ func _physics_process(delta):
 	var _direction_y = sign(mouse_position.y - global_position.y)
 	
 	if is_on_floor():
-		cur_jumps = 5
-		$"../GUI/UI".update_jumps(cur_jumps)
+		jumps = 5
+		gravity = 5000
+		$"../GUI/HUD".update_jumps(jumps)
 		animated_sprite.play("idle")
-	
+	elif velocity.y > 0:
+		gravity += 10
+		
 	# flying feels like giga shit lmao
-	if flying && cur_jumps != 0:
-		cur_jumps -= 1
-		$"../GUI/UI".update_jumps(cur_jumps)
+	if flying && jumps != 0 && can_fly:
+		jumps -= 1
+		$"../GUI/HUD".update_jumps(jumps)
 		velocity.x = cos(mouse_angle) * fly_force
 		velocity.y = sin(mouse_angle) * fly_force
+		can_fly = false
+		create_fly_timer(delta)
 		animated_sprite.play("flight")
 	
 	# update velocity
@@ -48,3 +49,7 @@ func _physics_process(delta):
 		animated_sprite.flip_h = true
 	else:
 		animated_sprite.flip_h = false
+
+func create_fly_timer(delta):
+	yield(get_tree().create_timer(.35), "timeout")
+	can_fly = true
