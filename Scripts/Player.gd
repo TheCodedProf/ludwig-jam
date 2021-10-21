b5ef09f42e36d6237c6315f476293001fe4776bc
Brian <brianmccollum@unm.edu>
2021-10-21 00:11:54 -0600

    fixed collision

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,10 @@
-extends "res://Scripts/Actor.gd"
+extends KinematicBody2D
 onready var animated_sprite = $AnimatedSprite
 onready var hitbox = $Hitbox
 
 var velocity = Vector2.ZERO # set velocity to o,o
 var max_run = 800 # max left and right speed
-var run_accel = 4800 # how fast you accelerate left and right
+var run_accel = 3800 # how fast you accelerate left and right
 var gravity = 6000 # overall gravity modifier (both jump and fall)
 var max_fall = 380 * 4# fall speed
 var jump_force = -240 * 4 # how high you jump
@@ -15,8 +15,7 @@ var local_hold_time = 0
 var cur_jumps = 5
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var on_ground = Game.check_walls_collision(self, Vector2.DOWN)
+func _physics_process(delta):
 	var flying = Input.is_action_just_pressed("wing_flap")
 	
 	# grabs mouse position
@@ -25,7 +24,7 @@ func _process(delta):
 	var direction_x = sign(mouse_position.x - global_position.x)
 	var _direction_y = sign(mouse_position.y - global_position.y)
 	
-	if on_ground:
+	if is_on_floor():
 		cur_jumps = 5
 		$"../GUI/UI".update_jumps(cur_jumps)
 		animated_sprite.play("idle")
@@ -42,21 +41,10 @@ func _process(delta):
 	velocity.x = move_toward(velocity.x, 0, run_accel * delta)
 	velocity.y = move_toward(velocity.y, max_fall, gravity * delta)
 	
-	# move player & check for collisions
-	move_x(velocity.x * delta, funcref(self, "on_collision_x"))
-	move_y(velocity.y * delta, funcref(self, "on_collision_y"))
+	velocity = move_and_slide(velocity, Vector2.UP, false, 2)
 	
 	# TODO: add player animation
 	if direction_x < 0:
 		animated_sprite.flip_h = true
 	else:
 		animated_sprite.flip_h = false
-	
-# collision functions
-func on_collision_x():
-	velocity.x = 0
-	zero_remainder_x()
-
-func on_collision_y():
-	velocity.y = 0
-	zero_remainder_y()
